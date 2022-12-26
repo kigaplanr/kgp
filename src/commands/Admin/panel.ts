622321f7f8b862ace19ab94cf1edb42316b8f430
Koni vKxni <eventbeate@gmail.com>
2022-12-26 09:35:27 +0100

    email panel

--- src/commands/Admin/panel.ts
+++ src/commands/Admin/panel.ts
@@ -56,6 +56,19 @@ export default new Command({
         },
       ],
     },
+    {
+      name: "email",
+      description: "Sends the email panel",
+      type: ApplicationCommandOptionType.Subcommand,
+      options: [
+        {
+          name: "channel",
+          description: "The channel to send the panel in",
+          type: ApplicationCommandOptionType.Channel,
+          required: false,
+        },
+      ],
+    },
   ],
   run: ({ interaction, client }) => {
     const channel =
@@ -68,7 +81,7 @@ export default new Command({
           .setLabel("Verifizieren")
           .setStyle(ButtonStyle.Primary)
           .setEmoji(`${emojis.id}`),
-          new ButtonBuilder()
+        new ButtonBuilder()
           .setCustomId("visitor-button")
           .setLabel("Besucher")
           .setStyle(ButtonStyle.Secondary)
@@ -187,6 +200,58 @@ export default new Command({
             },
           ])
       );
+      interaction.reply({ content: "Done!", ephemeral: true });
+      (channel as TextChannel).send({ embeds: [embed], components: [row] });
+    }
+
+    if (interaction.options.getSubcommand() === "email") {
+      const channel =
+        interaction.options.getChannel("channel") || interaction.channel;
+
+      const embed = new EmbedBuilder().setDescription(`
+      Willkommen auf dem **${interaction.guild?.name}** Server!\n
+      Hier sind alle wichtigen Informationen, Kontaktmöglichkeiten uvm. aufgelistet.
+      
+      **Allgemeines**
+      - Bei Fragen zu den Regeln, Befehlen oder ähnlichem, wende dich an einen der Moderatoren im Chat.
+      - Falls du einen Fehler jener Art in der Software entdeckst, melde ihn bitte direkt an die Entwickler.
+      - Wir helfen dir gerne bei allen Probleme weiter, beachte jedoch dass wir nicht direkt antworten können.
+
+      **Kontaktmöglichkeiten**
+      - Im Chat können aktive Teammitglieder (falls sie das wollen) direkt angesprochen werden.
+      - Du hast auch die Möglichkeit uns via privatem Ticket direkt zu kontaktieren.
+      - Als letzte Möglichkeit bieten wir E-Mail an, diese findest du unten.
+
+      **Registrierung und Verifizierungen**
+      - Du hast die Möglichkeit dich als offiziellen BAfEP Schüler zu verifizieren. 
+      - Als Schulsprecher kannst du auch deine Schule vertreten und Events ankündigen.
+      - Für Abschlussklassen gibt es die Möglichkeit ihre Umfragen der Diplomarbeit hier zu teilen.
+
+      **Datenschutz, Löschung und Co.**
+      - Deine Daten kannst du selbstverständlich jederzeit manuell und automatisiert löschen lassen. Da diese Vorgang aber viele Resourcen benötigt, kannst du dies maximal nur 1. tun.
+      - Falls deine Schule registriert ist, kannst du diese auf Anfrage komplett löschen lassen.
+      - Wir übernehmen keine Haftung über die ausversehene Weitergabe deiner Daten (Name, Fotos) die in Planungen etc. vorkommen, da diese nicht verschlüsselt sind.
+
+      **Weitere Informationen**
+      Nachrichten von mir (diesem "Bot") sind immer mit einem "🤖" gekennzeichnet, sie stellen keinen richtigen Nutzer dar.
+      Dieser wird jedoch von der Administration verwaltet, niemand anderes hat direkten Zugriff auf ihn - für die Sicherheit deiner Daten.
+      Bei offizieller Beantragung der Schließung, wird dieser Server ohne Vorwarnung geschlossen und mit hoher Wahrscheinlichkeit gelöscht.
+      `);
+
+      const row = new ActionRowBuilder<ButtonBuilder>().addComponents(
+        new ButtonBuilder()
+          .setCustomId("email-button")
+          .setLabel("Email anzeigen")
+          .setStyle(ButtonStyle.Primary)
+          .setEmoji("📧"),
+        new ButtonBuilder()
+          .setLabel("Link")
+          .setStyle(ButtonStyle.Link)
+          .setURL(
+            "https://discord.com/channels/1041005159477678170/1041294816471941262/1041300888716841042"
+          )
+      );
+
       interaction.reply({ content: "Done!", ephemeral: true });
       (channel as TextChannel).send({ embeds: [embed], components: [row] });
     }
