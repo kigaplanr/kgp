6f2d1ba7e0fd496d85e0a0676b42cff9b6095587
Koni vKxni <eventbeate@gmail.com>
2022-12-27 14:32:04 +0100

    typegoose warning

--- src/models/guild/autoresponse.ts
+++ src/models/guild/autoresponse.ts
@@ -1,9 +1,14 @@
-import { prop, getModelForClass } from "@typegoose/typegoose";
+import { prop, getModelForClass, modelOptions, Severity } from "@typegoose/typegoose";
 
 /**
  * @prop {string} guildID - The ID of the guild
 
  */
+@modelOptions({
+  options: {
+    allowMixed: Severity.ALLOW,
+  },
+})
 class Autoresponse {
   @prop()
   guildID: string;
